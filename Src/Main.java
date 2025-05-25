b79b7e6abc0a604a638c609265cf233856a2d907
Tplus <166980259+ctrlplust@users.noreply.github.com>
2025-05-25 06:08:24 +0000

    Generate Data

--- Src/Main.java
+++ Src/Main.java
@@ -2,20 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] palabras = {"Dragon", "Empire", "Quest", "Galaxy", "Legends", "Warrior"};
-        String[] categorias = {"Aventura", "Estrategia", "Accion"};
-        Random rand = new Random();
-        ArrayList<Game> juegos = new ArrayList<>();
-
-        // Generar 10000 juegos aleatorios
-        for (int i = 0; i < 10000; i++) {
-            String nombre = palabras[rand.nextInt(palabras.length)] + palabras[rand.nextInt(palabras.length)];
-            String categoria = categorias[rand.nextInt(categorias.length)];
-            int precio = 5000 + rand.nextInt(20000); // entre 5000 y 25000
-            int calidad = rand.nextInt(101); // entre 0 y 100
-            juegos.add(new Game(nombre, categoria, precio, calidad));
-        }
-
+        ArrayList<Game> juegos = GenerateData.generarJuegos(10000);
         Dataset dataset = new Dataset(juegos);
 
         // Medir tiempo de ordenamiento
